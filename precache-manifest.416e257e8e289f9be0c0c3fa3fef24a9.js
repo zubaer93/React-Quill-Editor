df277047a0ffdde9d1bedbd15be23c6f8d65dd70
zubaer <zubaer@stylinecollection.com>
2020-08-06 20:02:22 +0600

    Updates

---
+++ precache-manifest.416e257e8e289f9be0c0c3fa3fef24a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e533c83fb74b55578310b9d4db8d6208",
+    "url": "/react-quill-editor/index.html"
+  },
+  {
+    "revision": "bec134f952179a1853e6",
+    "url": "/react-quill-editor/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "682d4f26e19217659a87",
+    "url": "/react-quill-editor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bec134f952179a1853e6",
+    "url": "/react-quill-editor/static/js/2.52e866cd.chunk.js"
+  },
+  {
+    "revision": "9598fde2a22ec3ce6af9cd5c173a9b46",
+    "url": "/react-quill-editor/static/js/2.52e866cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682d4f26e19217659a87",
+    "url": "/react-quill-editor/static/js/main.6a96f418.chunk.js"
+  },
+  {
+    "revision": "2a58581c6ebe85dc4d8e",
+    "url": "/react-quill-editor/static/js/runtime-main.9eff8080.js"
+  }
+]);
